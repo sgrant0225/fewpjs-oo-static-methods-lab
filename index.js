63196b079f2a519da20cd44a448b03ad08982188
github username <sharnellgrant@gmail.com>
2021-08-10 10:55:19 -0400

    commit tree 45c19ff6c9831b512fd25f4d2d7a8f742646a3eb

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+  static capitalize (word) {
+     return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize (stringWords) {
+    return stringWords.replace( /[^A-Za-z0-9 '-]/g, '' ) 
+  }
+
+  static titleize () {
+    
+  }
 }
